b8361301652bf4eadb760f40a7050179b1b5a83c
MasterMek35 <MasterMek35@users.noreply.github.com>
2026-09-01 11:23:10 -0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.mek35.spacedefender"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mek35.spacedefender"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
